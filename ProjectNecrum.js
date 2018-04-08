b76fdcaec0aefba1eeef1bfa821ecd98fd66a8cf
Mathue24 <Mathue24@gmail.com>
2018-04-08 22:43:38 +0200

    dasdas

--- ProjectNecrum.js
+++ ProjectNecrum.js
@@ -39,7 +39,7 @@ bot.on('message', message => {
             return message.reply("Don't talk to me you pathetic worm!");
 
         var member= message.mentions.members.first();
-        message.channel.send(`${user.mention()} message text`);
+        bot.sendMessage({to: channelID, message: `${user.mention()} message text`});
         // message.channel.send(member.displayName);
         // message.channel.send(member.message.author);
     }
