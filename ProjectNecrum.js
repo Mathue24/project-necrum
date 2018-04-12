15342ce968c9b677792dedd5f31e68530aa3f11a
Mathue24 <Mathue24@gmail.com>
2018-04-13 00:35:04 +0200

    Purging Update!

--- ProjectNecrum.js
+++ ProjectNecrum.js
@@ -61,9 +61,32 @@ bot.on('message', message => {
         message.channel.send(':crab:CRABS ARE PEOPLE:crab:CLAMS ARE PEOPLE:crab:LEGIT OR QUIT:crab:');
     }
 
-    else if(msg.includes('test'))
+    else if(msg.startsWith(prefix + 'PURGe'))
     {
-        message.channel.send('Yeh');
+        async function purge() 
+        {
+            message.delete();
+            
+            if(!message.member.roles.some(r=>["Administrator", "Member of The Party"].includes(r.name)) )
+            {
+                return message.reply('Do not talk to me you pathetic worm! Only a \'Member of The Party\' can use this command!');
+            }
+
+            if(isNaN(args[0]))
+            {
+                message.channel.send('Please use a number as your argument. \n Usage: '+ prefix +'purge <ammount>');
+                return;
+            }
+
+            const fetched = await message.channel.fetchMessage({limit: args[0]});
+            console.log(fetched.size + ' messages found, deleting...');
+
+            message.channel.bulkDelete(fetched)
+                .catch(error => message.channel.send('Error: ${error}'));
+                
+        }
+
+        purge();
     } 
 
 
